bd9d7a326ded4625dc05cd40bcd0e979711833f5
bboampong <baffouraduboampong@gmail.com>
2018-05-18 23:02:05 +0000

    Forcing SSL in priduction

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -16,6 +16,11 @@ class AppServiceProvider extends ServiceProvider
     {
         //
 	Schema::defaultStringLength(191);
+
+	// Force SSL in production
+	if ($this->app->environment() == 'production') {
+	    URL::forceScheme('https');
+	}
     }
 
     /**
