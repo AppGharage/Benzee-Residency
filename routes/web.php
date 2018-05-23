d2366e31ac32d7457c34d48208137a378e1f0785
bboampong <baffouraduboampong@gmail.com>
2018-05-23 20:10:50 +0000

    Added New Request View Route

--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,4 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/requests', 'HomeController@index')->name('request.index');
