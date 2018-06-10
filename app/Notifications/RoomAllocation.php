34726d53ac0d6fb4def59cc74b757daa0c40d316
bboampong <baffouraduboampong@gmail.com>
2018-06-10 18:14:43 +0000

    Updated RoomAllocation Notification Message

--- app/Notifications/RoomAllocation.php
+++ app/Notifications/RoomAllocation.php
@@ -53,8 +53,8 @@ class RoomAllocation extends Notification implements ShouldQueue
                     ->line($this->userDetails->fullname. ', this is to inform you that you have been allocated a '.
                     $this->bookingDetails->request->occupancy_type.
                     '. You are therefore required to make payment of your Rent (Hostel Fees) which is $' . $this->bookingDetails->amount . ' (USD) for the duration of '.
-                    $this->bookingDetails->request->duration .' before ' . $this->bookingDetails->fee_end_date->format('F d, Y'))
-                    ->line('We only accept Cash being full payment or half payment of your Rent within 30 days and the remaining half paid within 30 days days of initial payment.')
+                    $this->bookingDetails->request->duration .' before ' . $this->bookingDetails->fee_end_date->format('F d, Y') . '.')
+                    ->line('We only accept Cash being full payment of your Rent within 30 days.')
                     ->line('Kindly note that, your may loose your accommodation if payment is not made within 30 days. We also wish to remind you that the Rent excludes your Electricity Bill fro your stay.')
                     ->line('Payment of Rent(Hostel Fee) is currently by cash at the premises of the Residency to Management. You may locate the Residence by clicking the button below')
                     ->action('Locate BenZee Residency', url('https://goo.gl/maps/JaFbTCcfMym'))
