913a9db333cd71c462df3afa6d56d9cbb6284998
bboampong <baffouraduboampong@gmail.com>
2018-06-12 19:28:38 +0000

    Updated Booking Fee Confirmation

--- app/Notifications/BookingSent.php
+++ app/Notifications/BookingSent.php
@@ -59,6 +59,19 @@ class BookingSent extends Notification implements ShouldQueue
                     $this->bookingDetails->end_date->format('F d, Y'). ' . The booking fee after payment of your
                      rent will be used as a deposit against damages during your stay. To pay your booking, kindly click the Pay Booking button below
                      or with the link => '. url('/booking/pay/'.$this->bookingDetails->id))
+                     ->line(' ')
+                    ->line('Below are the steps to pay your Booking Fee; ')
+                    ->line('1. Get your Mobile Money Registered Handset/Phone Ready. 
+                        (You will need a Registered Mobile Money Number to make payment)')
+                    ->line('2. Top Up your Mobile Money account with GHS 210.00 . ')
+                    ->line('3. Click on the Pay booking button or use the booking link provided in either the SMS or EMail')
+                    ->line('4. Fill the payment details by selecting the Network Operator of your Mobile Money Account,
+                         with the Name and Telephone number of your Mobile Money Account.')
+                    ->line('5. Complete the Transaction on your mobile Money Handset by authorizing the payment on the 
+                        confirmation screen on your Mobile Money handset')
+                    ->line('Kindly Note that in a case where you do not have a mobile money account, A family or a friend with a Mobile Money account
+                    can make payment on your behalf with the Booking link and the Instructions above. ')
+                    ->line(' ')
                     ->action('Pay Booking', url('/booking/pay/'.$this->bookingDetails->id))
                     ->line('Thank you for expressing an interest in us!');
     }
